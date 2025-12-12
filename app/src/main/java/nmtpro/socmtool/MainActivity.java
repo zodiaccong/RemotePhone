a8942002207fb39f9e0a6bc461b59666453f13cd
zodiaccong <52649950+zodiaccong@users.noreply.github.com>
2025-12-12 14:21:18 +0700

    Update MainActivity.java

--- app/src/main/java/nmtpro/socmtool/MainActivity.java
+++ app/src/main/java/nmtpro/socmtool/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         initializeUI();
         setupEventListeners();
         requestPermissions();
+        loadServerConfig();
     }
 
     private void initializeUI() {
